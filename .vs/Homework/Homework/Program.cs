6c7787687e462335c48977220279177c7d13f497
Yulia <yulia.menshykh@gmail.com>
2022-02-08 20:39:54 +0200

    new homework

---
+++ .vs/Homework/Homework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Homework 1
+
+            int res = sizeof(int);
+            Console.WriteLine(res);
+
+            int res2 = sizeof(char);
+            Console.WriteLine(res2);
+        }
+    }
+}

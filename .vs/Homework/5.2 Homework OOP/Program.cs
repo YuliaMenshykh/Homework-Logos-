116a32179c39a463858fe888700b4c64c4c5e6f9
yulia menshykh <yulia.menshykh@gmail.com>
2022-02-22 16:55:03 +0200

    OOP  Homework  5.1  and 5.2

---
+++ .vs/Homework/5.2 Homework OOP/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2_Homework_OOP
+{
+    class Name
+    {
+        public string firstName;
+        public string lastName;
+        public string position;
+        public string function;
+        public void printShopper()
+        {
+            Console.WriteLine($"FirstName: {firstName}");
+            Console.WriteLine($"LastName: {lastName}");
+        }
+        public void printWorkers()
+        {
+            Console.WriteLine($"FirstName: {firstName}");
+            Console.WriteLine($"LastName: {lastName}");
+            Console.WriteLine($"Position: {position}");
+            Console.WriteLine($"Function: {function}");
+        }
+    }
+
+
+    class Program
+    {
+        static Name GetShopper()
+        {
+            Name name = new Name();
+            name.firstName = "Bob";
+            name.lastName = "Marley";
+            return name;
+        }
+
+        static Name GetMenadger()
+        {
+            Name name = new Name();
+            name.firstName = "Dan";
+            name.lastName = "Taller";
+            name.position ="menager";
+            name.function = "Hiring, training, supervising, and evaluating employees.\n Handling customer complaints.";
+            return name;
+        }
+
+        static Name GetAccountant()
+        {
+            Name name = new Name();
+            name.firstName = "Emily";
+            name.lastName = "Tersis";
+            name.position = "accountant";
+            name.function = "Ensure timely bank payments \n Compute taxes and prepare tax returns \n Handle monthly, quarterly and annual closings";
+            return name;
+        }
+
+        static Name GetCashier()
+        {
+            Name name = new Name();
+            name.firstName = "Polly";
+            name.lastName = "Sayti";
+            name.position = "Cashier";
+            name.function = "Manage transactions with customers using cash registers.\n Handle merchandise returns and exchanges \n Maintain clean and tidy checkout areas ";
+            return name;
+        }
+
+        static void Main(string[] args)
+        {
+            Name FirstShopper = GetShopper();
+            Console.WriteLine("\n = Shopper =");
+            FirstShopper.printShopper();
+            Name Cashier = GetCashier();
+            Console.WriteLine("\n = Cashier =");
+            Cashier.printWorkers();
+            Name Accountant = GetAccountant();
+            Console.WriteLine("\n = Accountant =");
+            Accountant.printWorkers();
+            Name Menadger = GetMenadger();
+            Console.WriteLine("\n = Menager =");
+            Menadger.printWorkers();
+        }
+    }
+}
+ 
---
+++ .vs/Homework/Homework OOP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_OOP
+{
+
+    class Cat
+    {
+        public void Speek()
+        {
+            Console.WriteLine("- Meow -_-");
+        }
+    }
+    class Human
+    {
+        public void PettingCat()
+        {
+            Console.WriteLine("- Dogs are better than cats !");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Human human = new Human();
+            human.PettingCat();
+            cat.Speek();
+
+        }
+    }
+}

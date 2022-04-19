2e5269b64c8a03ff5f6e80a1c6ad3c53529623d2
yulia menshykh <yulia.menshykh@gmail.com>
2022-04-19 17:58:08 +0300

    HW 8.3

---
+++ .vs/Homework/HW  7.1 student assessment/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW__7._1_student_assessment
+{
+    class Program
+    {
+        class StudClass
+        {
+            class Student
+            {
+                public int[] points { get; set; }
+                public double avgPoint
+                {
+                    get
+                    {
+                        double sum = 0;
+                        for (var i = 0; i < points.Length; i++)
+                        {
+                            sum += points[i];
+                        }
+                        return sum / points.Length;
+                    }
+                }
+            }
+
+            class StudentAuditory
+            {
+                public Student[] students { get; set; }
+                public static StudentAuditory generateRandomStudAuditory()
+                {
+                    var rnd = new Random();
+                    int studentCount = rnd.Next(5, 10);
+                    var students = new List<Student>();
+
+                    for (var i = 0; i < studentCount; i++)
+                    {
+                        var rnd2 = new Random();
+                        students.Add(new Student()
+                        {
+                            points = new int[3] { rnd2.Next(1, 6), rnd2.Next(1, 6), rnd2.Next(1, 6) }
+                        });
+                    }
+
+                    return new StudentAuditory() { students = students.ToArray() };
+                }
+            }
+            public static void run()
+            {
+                var rndStudentAud = StudentAuditory.generateRandomStudAuditory();
+
+                foreach (var item in rndStudentAud.students)
+                {
+                    Console.WriteLine($"{item.points[0]}, {item.points[1]}, {item.points[2]}");
+                    Console.WriteLine(item.avgPoint);
+                    Console.WriteLine();
+                }
+
+            }
+        }
+    }
+}
--- .vs/Homework/HW 8.3 generic/Program.cs
+++ .vs/Homework/HW 8.3 generic/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace HW_8._3_generic
 {
-   
-   class Program
-   {
-      
-   }
+    class Person<I, P>
+    {
+        public I Id { get; }
+        public P Password { get; set; }
+        public string Name { get; }
+        public Person(I id, P password, string name)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+        }
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           Person<int, string> Vlad = new Person<int, string>(3323,"Q12", "Vlad");
+           Console.WriteLine(Vlad.Id);
+        }
+    }
 
 }

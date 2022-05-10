4e2915eb5128ac4e0f0019a2bca1b0492cd4c6f3
yulia menshykh <yulia.menshykh@gmail.com>
2022-05-10 23:40:35 +0300

    HW 9.2

---
+++ .vs/Homework/HW 9.2 recursion/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_9._2_recursion
+{
+    class Program
+    {
+        class Welcome
+        {
+            public Welcome()
+            {
+                Console.WriteLine("Hi" +
+                    "\n." +
+                    "\n." +
+                    "\n." +
+                    "\n." +
+                    "\n.");
+            }
+
+
+            public Welcome(int a)
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    Console.WriteLine("Goodbye" +
+                    "\n_" +
+                    "\n_" +
+                    "\n_" +
+                    "\n_");
+                }
+                
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Welcome ss = new Welcome(5);
+                Welcome s1 = new Welcome();
+            }
+           
+        }
+    }
+}
